9099c9c5873fa1a8c8fdc4dd8b6f97f16d418fb4
Antonio Solano <jsolano91@gmail.com>
2016-04-18 23:03:03 -0600

    fixed zoom

--- dengued3/app.js
+++ dengued3/app.js
@@ -28755,6 +28755,7 @@ leaflet.tileLayer(
 /* Initialize the SVG layer */
 map._initPathRoot()
 
+var zoom = map.getZoom(zoom)
 
 /* We simply pick up the SVG from the map object */
 var svg = d3.select("#map").select("svg"),
@@ -28773,6 +28774,7 @@ var tooltip = d3.select("body").append("div")
 
 var variableWidthSTR = d3.select("body").style("width");
 var variableWidth = parseInt(variableWidthSTR.substring(0, variableWidthSTR.length - 2));
+
 
 
 d3.json("data/locations.json", function(collection) {
@@ -28816,19 +28818,18 @@ d3.json("data/locations.json", function(collection) {
 			.style("opacity", 0)
 		});
 
-
 	map.on("viewreset", update);
 	update();
 
 	function update() {
 		feature
-		// .transition().duration(2000).delay(function(d, i) {return i*2})
-		.attr("transform",
-		function(d) {
-			return "translate("+
-			  map.latLngToLayerPoint(d.LatLng).x +","+
-			  map.latLngToLayerPoint(d.LatLng).y +")";
-		})
+			.attr("transform",
+				function(d) {
+					return "translate("+
+					  map.latLngToLayerPoint(d.LatLng).x +","+
+					  map.latLngToLayerPoint(d.LatLng).y +")";
+				})
+			.attr("r",function(d) { return radiusScale(d.casos)/400*Math.pow(2,map.getZoom())})
 	}
 })
 
