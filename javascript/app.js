be89e64df0a96d68b3b2175db082fa96662aaa9f
Antonio Solano <jsolano91@gmail.com>
2016-04-18 22:27:05 -0600

    Commit the project for the third time

--- javascript/app.js
+++
@@ -1,8 +0,0 @@
-// Declare dependencies
-var d3 = require('d3')
-var leaflet = require('leaflet')
-window.d3 = d3
-
-// Require charts
-var map = require('./map.js');
-var chart = require('./chart.js');
